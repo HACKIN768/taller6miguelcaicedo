a4674fe1ff8b1763d230975ed0133f7965acb0ca
Estudiante USC <estudiante@GTACAD.USC>
2023-03-24 16:14:30 -0500

    taller6

---
+++ Taller 6 miguelcaicedo1/src/taller6/puntoCuatro.java
@@ -0,0 +1,10 @@
+package taller6;
+public class puntoCuatro {
+    public static void main(String[] args) {
+        byte[] valores = new byte[10];
+        
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = 1;
+        }
+    }
+}
